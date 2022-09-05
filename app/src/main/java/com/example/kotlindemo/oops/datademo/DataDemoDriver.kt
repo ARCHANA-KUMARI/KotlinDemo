17cff7a757d30c6067db9e5465c878c73722eab0
Archana Kumari <ms.archanacusat2015@gmail.com>
2022-09-05 17:12:26 +0530

    Data class propertes demo added

--- app/src/main/java/com/example/kotlindemo/oops/datademo/DataDemoDriver.kt
+++ app/src/main/java/com/example/kotlindemo/oops/datademo/DataDemoDriver.kt
@@ -11,6 +11,8 @@ fun main(args : Array<String>){
     // Data class compare object by using content in both case equals and ==
     println("DataDemo object equals Method check" + dataDemo.equals(dataDemo1))
     println("DataDemo object == operator check" + (dataDemo == dataDemo1))
+    println("DataDemo object hashCode Method check" + dataDemo.hashCode()) // same hashcode of dataDemo1
+    println("DataDemo1 object hashCode Method check" + dataDemo1.hashCode()) // same hashcode of dataDemo
 
     var dataDemo2 = DataDemo("abcTest", 7 )
     println("DataDemo object equals Method check false demo" + dataDemo.equals(dataDemo2))
@@ -24,4 +26,10 @@ fun main(args : Array<String>){
 
 
     println("DataDemo componentN" + dataDemoCopyOf)
+
+   /* // Demo for properties which is not defined in primary constructor
+    var dataDemoMis = DataDemoMis("DevK")
+    println("DataDemoMis object" + dataDemoMis)
+    println("DataDemoMis toString" + dataDemoMis.toString())
+    println("DataDemoMis hashCode" + dataDemoMis.hashCode())*/
 }
---
+++ app/src/main/java/com/example/kotlindemo/oops/datademo/DataDemoMis.kt
@@ -0,0 +1,10 @@
+package com.example.kotlindemo.oops.datademo
+
+/**
+ * @Author: Archana Kumari
+ * @Date: 05-09-2022
+ */
+data class DataDemoMis(val name: String){
+    var age : Int = 0
+}
+
