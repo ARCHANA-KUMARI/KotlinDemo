86fdd065cf93c2c307eec44c61da86b859eaf4eb
Archana Kumari <ms.archanacusat2015@gmail.com>
2022-09-17 15:27:28 +0530

    Sealed interface demo

---
+++ app/src/main/java/com/example/kotlindemo/SealedImplementor.kt
@@ -0,0 +1,15 @@
+package com.example.kotlindemo
+
+import com.example.kotlindemo.oops.sealed.SealedInterfaceDemo
+
+/**
+ * This implementor class is defiend out side of package/module so its giving complie time error.
+ * @Author: Archana Kumari
+ * @Date: 17-09-2022
+ */
+/*
+class SealedImplementor : SealedInterfaceDemo{
+    */
+/*Inheritor of sealed class or interface declared in package com.example.kotlindemo but it must be in package com.example.kotlindemo.oops.sealed where base class is declared*//*
+
+}*/
