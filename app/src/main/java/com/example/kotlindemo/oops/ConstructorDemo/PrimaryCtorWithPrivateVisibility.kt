e0766dd470c91625e1531b451e2c3345c12c20fa
Archana Kumari <ms.archanacusat2015@gmail.com>
2022-09-17 20:33:08 +0530

    Primary ctor with private visibility

---
+++ app/src/main/java/com/example/kotlindemo/oops/ConstructorDemo/PrimaryCtorWithPrivateVisibility.kt
@@ -0,0 +1,9 @@
+package com.example.kotlindemo.oops.ConstructorDemo
+
+/**
+ * If you don't want your class to have a public constructor, declare an empty primary constructor with non-default visibility:
+ * @Author: Archana Kumari
+ * @Date: 17-09-2022
+ */
+class PrimaryCtorWithPrivateVisibility private constructor() {
+}
