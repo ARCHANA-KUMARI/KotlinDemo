07ad690b076229a9c93746f392e38cc96206e04d
Archana Kumari <ms.archanacusat2015@gmail.com>
2023-03-25 13:04:51 +0530

    Added live data and data binding

--- app/src/main/java/com/example/kotlindemo/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/kotlindemo/viewmodel/MainActivityViewModel.kt
@@ -1,16 +1,34 @@
 package com.example.kotlindemo.viewmodel
 
-import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.concurrent.thread
 
 /**
  * @Author: Archana Kumari
  * @Date: 23-03-2023
  */
-public class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel : ViewModel() {
+    private var counter = MutableLiveData(0)
+    val counters: LiveData<Int> = counter
+    fun getCounter(): Int? {
+        return counter.value
+    }
+
     fun onClickOfBlockUiThread() {
         while (true) {
-            println("Blocking UI thread")
+            //println("Blocking UI thread")
+            // Inside thread
+            thread(start = true) {
+                println("Current Thread :" + Thread.currentThread().getName())
+                println("Blocking UI thread")
+            }
         }
+    }
+
+    fun onClickOfCounterButton() {
+        counter.value = (counter.value)?.plus(1)
+        println("OnClick of counter" + counter.value)
     }
 }
