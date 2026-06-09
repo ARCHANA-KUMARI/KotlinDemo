0c868a47b79edea0fc856884a5b9f2770f2a60fb
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-06-09 16:45:36 +0530

    Inline function with demo

--- app/src/main/java/com/example/kotlindemo/oops/Inline/inlineFunctionBasic.kt
+++ app/src/main/java/com/example/kotlindemo/oops/Inline/inlineFunctionBasic.kt
@@ -1,9 +1,13 @@
 package com.example.kotlindemo.oops.Inline
 
 fun main() {
- messageInlineDemo { println("Demo inline function") }
+    messageInlineDemo {
+        println("Demo inline function")
+        return
+    }
+    messageInlineDemo { println("Demo inline function after return") }
 }
 
-inline fun messageInlineDemo(a:() -> Unit){
+inline fun messageInlineDemo(a: () -> Unit) {
     a.invoke()
 }
