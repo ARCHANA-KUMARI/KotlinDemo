00cfaa811be083303fc11482a34b85e2b832f699
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-08-24 21:00:20 +0530

    Lambda demo

---
+++ app/src/main/java/com/example/kotlindemo/concept/highorderfn/LambdaDemo.kt
@@ -0,0 +1,16 @@
+package com.example.kotlindemo.concept.highorderfn
+
+fun main() {
+    val sum = { a: Int, b: Int -> a + b }
+    val multiply = { a: Int, b: Int -> a * b }
+
+    println("Sum of 2 and 3 is ${sum(2, 3)}")
+    println("Multiplication of 2 and 3 is ${multiply(2, 3)}")
+
+    // Void multiline lambda and it`s return type is Unit and it has no parameters and multilineLa
+    val multilineLambda:()->Unit = {
+        println("This is a multiline lambda")
+        println("It can have multiple statements")
+    }
+    multilineLambda()
+}
