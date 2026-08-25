fde3eef77a4e0a51ce7abeeae95ade109367d4e7
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-08-25 18:08:07 +0530

    Higher order function with collection

---
+++ app/src/main/java/com/example/kotlindemo/concept/highorderfn/CollectionWithOrderFn.kt
@@ -0,0 +1,12 @@
+package com.example.kotlindemo.concept.highorderfn
+
+fun isOdd(num: Int): Boolean {
+    return num % 2 != 0
+}
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val oddNumbers =
+        numbers.filter(::isOdd) // passing normal function as an argument to the filter function
+    println(oddNumbers) // Output: [1, 3, 5, 7, 9]
+}
